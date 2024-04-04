ee838de2f258912654ead06400d2cf131725b8a1
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-04-04 19:07:39 -0400

    menu bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import './Canvas.js';
 
 function App() {
   return (
-    <div>TESTING</div>
+    <div>
+      <div className='menu-bar'>
+        <div className='menu-logo'>The Echochrome Project</div>
+        <div className='menu-items'>
+          <div className='menu-item'>paint!</div>
+          <div className='menu-item'>about</div>
+          <div className='menu-item'>resources</div>
+        </div>
+      </div>
+      <div className='home-content'>
+          Grid function here
+          
+      </div>
+    </div>
   );
 }
 
