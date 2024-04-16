8b9b48df38335503e0f6f69b3e56ac352205eab3
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-04-16 15:13:44 -0400

    resources links

--- src/About.js
+++ src/About.js
@@ -12,6 +12,7 @@ function About() {
         const item3 = document.getElementById('item-3');
         const subheader1 = document.getElementById('subheader-1');
         const subheader2 = document.getElementById('subheader-2');
+        const subheader3 = document.getElementById('subheader-3');
         const creditText = document.getElementById('credit-text');
         const lightBulbWrapper = document.getElementById('light-bulb-wrapper');
         const plantIcon = document.getElementById('plant-icon');
@@ -27,6 +28,7 @@ function About() {
             item3.style.color = '';
             subheader1.style.color = '';
             subheader2.style.color = '';
+            subheader3.style.color = '';
             creditText.style.color = '';
             lightBulbWrapper.style.borderColor = '';
             plantIcon.style.fill = '';
@@ -41,6 +43,7 @@ function About() {
             item3.style.color = '#267DFF';
             subheader1.style.color = '#267DFF';
             subheader2.style.color = '#267DFF';
+            subheader3.style.color = '#267DFF';
             creditText.style.color = '#267DFF';
             lightBulbWrapper.style.borderColor = 'black';
             plantIcon.style.fill = '#267DFF';
@@ -72,7 +75,7 @@ function About() {
                         <path id="plant-icon" fill-rule="evenodd" clip-rule="evenodd" d="M100 0.571426H90V10.5714H80V20.5714H70V30.5714V40.5714V50.5714H60V40.5714H50V30.5714H40H30H20V40.5714H10V50.5714H0V60.5714V70.5714H10V60.5714H20H30H40H50V50.5714H60V60.5714H70V70.5714V80.5714V90.5714V100.571V110.571H80V100.571V90.5714V80.5714V70.5714V60.5714V50.5714V40.5714H90H100H110V30.5714H120H130V20.5714V10.5714H140V0.571426H130H120H110H100Z" fill="#F2FF59" />
                     </svg>
 
-                    <div className='subheader' id="subheader-1"> WHAT IS THIS???</div>
+                    <div className='subheader' id="subheader-1">WHAT IS THIS???</div>
                     <div className='paragraph-text'>The Echochrome Project is a web-based interactive experience
                         that helps graphic designers, web developers and UI/UX designers visualise how
                         their creative decisions (like picking a colour palette) can have an impact on the environment.
@@ -83,7 +86,34 @@ function About() {
                         and thereby contribute to the overall eco-friendliness of the device.
                     </div>
 
-                    <div className='subheader' id="subheader-2">WHAT IS THAT PAINT TOOL?</div>
+                    <div className='subheader' id="subheader-2">MY INSPIRATION</div>
+                    <div className='paragraph-text'>
+                        As an aspiring UI/UX developer, I wondered how designers and visual creators
+                        could play their part in driving an effort against today's environmental crisis
+                        and spread awareness about the impact of the technological industries on the climate.
+                        As a designer and developer, I love being able to harness the power of cool new tech,
+                        but I didn't realise the part it plays in the global environment, and the ways in which
+                        our small actions can lead to larger consequences.
+                        <br></br>
+                        <br></br>
+                        So, I decided to create the <b>Ecochrome Project</b> as a <i>Speculative Future tool</i>,
+                        that can help us visualise ways in which designers and front-end developers can be more
+                        aware of the decisions they make and the ecological impact it can have.
+                        <br></br>
+                        <br></br>
+                        This project focuses on <b>colour</b>! Because that's where most creatives will start when
+                        they begin a project. My tool aims to help you see how a seemingly small decision; like what
+                        colour palette you should use for your fun new app, can equate to something more... In this case
+                        that's energy consumption on an OLED screen.
+                        <br></br>
+                        <br></br>
+                        While not all screens are OLED, most modern day phones
+                        and laptops are shifting towards this kind of screen-based technology to help optimise power usage.
+                        I hope this tool can help you learn something new today, and can be a small part in creating a
+                        larger conversation about environmental activism in design and development!
+                    </div>
+
+                    <div className='subheader' id="subheader-3">WHAT IS THAT PAINT TOOL?</div>
                     <div className='paragraph-text'>The <Link to="/paint">Paint tool</Link> was a way to visualise and spread awareness
                         about how visual designers and developers can help contribute towards a greener future.
                         <br></br>
--- src/Resources.js
+++ src/Resources.js
@@ -2,23 +2,106 @@ import './App.css';
 import { Link } from "react-router-dom";
 
 function Resources() {
-  return (
-    <div>
-      <div className='menu-bar'>
-        <div className='menu-logo'>
-          The Ecochrome Project
-          </div>
-        <div className='menu-items'>
-          <Link className='menu-item' to="/paint">paint!</Link>
-          <Link className='menu-item' to="/about">about</Link>
-          <Link className='menu-item' to="/resources">resources</Link>
+    return (
+        <div>
+            <div className='menu-bar'>
+                <div className='menu-logo'>
+                    The Ecochrome Project
+                </div>
+                <div className='menu-items'>
+                    <Link className='menu-item' to="/paint">paint!</Link>
+                    <Link className='menu-item' to="/about">about</Link>
+                    <Link className='menu-item' to="/resources">resources</Link>
+                </div>
+            </div>
+            <div className='home-content'>
+                <div className='text-block'>
+                    <svg width="100" viewBox="0 0 200 222" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M127.69 62.4428C185.55 -4.72255 242.015 94.6264 155.574 110.718C242.015 127.509 184.853 226.159 127.69 158.993C156.968 242.949 43.3391 242.25 72.6179 158.993C14.7575 226.159 -41.7084 127.509 44.7333 110.718C-42.4055 94.6264 14.7575 -4.02293 72.6179 62.4428C42.642 -20.8142 156.968 -20.8142 127.69 62.4428ZM99.8048 98.1246C106.776 98.1246 113.05 103.722 113.05 110.718C113.05 117.714 106.776 124.011 99.8048 124.011C92.834 124.011 87.2566 117.714 87.2566 110.718C87.2566 103.722 92.834 98.1246 99.8048 98.1246Z" fill="#EA89E6" />
+                    </svg>
+
+                    <div className='subheader large pink'>BLOOMING BUDS</div>
+                    <div className='caption pink'>The inspiration.</div>
+                    <div className='paragraph-text'>
+                        I was inspired by several articles and projects that aimed to discuss the role of
+                        designers and developers as environmental allies. The Ecochrome project's main tool was
+                        inspired by the idea of click-to-paint flash games and tools like Microsoft's Paint to show
+                        the users how different colours impact the overall energy being consumed by their canvas.
+                    </div>
+
+                    <svg width="100" viewBox="0 0 201 201" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M100.818 0.499954C74.2798 0.499954 52.7664 22.0135 52.7664 48.5519C52.7664 50.7036 51.022 52.448 48.8703 52.448C22.3319 52.448 0.818359 73.9614 0.818359 100.5C0.818359 127.038 22.3319 148.552 48.8703 148.552C51.022 148.552 52.7664 150.296 52.7664 152.448C52.7664 178.986 74.2798 200.5 100.818 200.5C127.357 200.5 148.871 178.986 148.871 152.448C148.871 150.296 150.615 148.552 152.766 148.552C179.305 148.552 200.818 127.038 200.818 100.5C200.818 73.9614 179.305 52.448 152.766 52.448C150.615 52.448 148.871 50.7036 148.871 48.5519C148.871 22.0135 127.357 0.499954 100.818 0.499954Z" fill="#F2FF59" />
+                    </svg>
+
+                    <div className='subheader large'>BRANCHING OUT</div>
+                    <div className='caption'>The design.</div>
+                    <div className='paragraph-text'>
+                        Several design decisions drove the creation of this simple website!
+                        <ul>
+                            <li>The font I chose to use is <b>Lucida Console</b>. This font
+                                was designed to be extremely legible on a low-resolution display, and
+                                one of the font designers was a woman. This font is also a system font and
+                                is an ode to the text style of CRT monitors.
+                            </li>
+
+                            <li>I've only used six colours on this website to maintain a simple
+                                dark mode theme. While dark mode is great for being energy efficient,
+                                it's important to never use white text on a pure black background (to be gentle on the eyes).
+                                For this reason, I chose to use a deep green-grey background instead, with a bamboo green highlight colour
+                                and a fuschia pink for accents.
+                            </li>
+
+                            <li>The code used on this website is entirely my own, but I did use several open-source references which are
+                                linked below! Feel free to inspect this website's code!
+                            </li>
+
+                            <li>This site was prototyped on <a href="https://www.figma.com/file/tytFa2L0y8gD4eiCNhyo1f/Assignment-4-Mockup?type=design&node-id=0%3A1&mode=design&t=8VtjYcAFBHsWL0oC-1">Figma</a></li>
+                        </ul>
+                    </div>
+
+                    <svg width="100" viewBox="0 0 201 126" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M166.468 44.2721C166.89 42.033 167.111 39.7227 167.111 37.3602C167.111 17.0522 150.807 0.589333 130.694 0.589333C118.388 0.589333 107.508 6.75255 100.916 16.1889C94.5877 10.8243 86.4252 7.59332 77.5156 7.59332C57.5963 7.59332 41.4124 23.7415 41.1037 43.7801H41.0992C18.7522 43.7801 0.636719 62.0721 0.636719 84.6368C0.636719 107.202 18.7522 125.494 41.0992 125.494H160.175C182.521 125.494 200.637 107.202 200.637 84.6368C200.637 64.2338 185.826 47.325 166.468 44.2721Z" fill="#7E9AFF" />
+                    </svg>
+
+                    <div className='subheader large blue'>WATERING MY GARDEN</div>
+                    <div className='caption blue'>The references and resources.</div>
+                    <div className='paragraph-text'></div>
+                    Articles
+                    <ul>
+                        <li><a href="https://consider.digital/low-carbon-web-colour-palettes/">Low-Carbon Web Colour Palettes: Why Black Is The New Green</a></li>
+                        <li><a href="https://www.fastcompany.com/90605005/these-are-the-worlds-most-sustainable-fonts">Sustainable Fonts</a></li>
+                        <li><a href="https://greentheweb.com/energy-efficient-color-palette-ideas/">Energy Efficient Color Palettes</a></li>
+                    </ul>
+                    <br></br>
+                    <br></br>
+                    Projects
+                    <ul>
+                        <li><a href="https://sustainablewebdesign.org/calculating-digital-emissions/">Sustainable Web Design: Calculating Digital Emissions</a></li>
+                        <li><a href="https://api.websitecarbon.com/#site-endpoint">Website Carbon API</a></li>
+                        <li><a href="https://cyberfeminismindex.com">Cyberfeminism Index</a></li>
+                        <li><a href="https://illuminating-radioactivity.com/">Illuminating Radioactivity</a></li>
+                    </ul>
+                    <br></br>
+                    <br></br>
+                    Books
+                    <ul>
+                        <li>Japanese Color Harmony Dictionary - Traditional Colors of Japan: The Complete Guide for Designers and Graphic Artists
+                            <br></br>
+                            <i>By Teruko Sakurai (2021)</i>
+                        </li>
+                    </ul>
+                    <br></br>
+                    <br></br>
+                    Coding resources
+                    <ul>
+                        <li><a href="https://learnersbucket.com/examples/interview/convert-hex-color-to-rgb-in-javascript/">Hexadecimal to RGB</a></li>
+                        <li>Mozilla Web Docs</li>
+                        <li>W3Schools</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className='home-content'>
-          
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Resources;
