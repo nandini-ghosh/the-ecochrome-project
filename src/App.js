e40ea35c563e8b48242c278aa4d1d030386f0567
nandini-ghosh <nandini.ghosh2094@gmail.com>
2024-04-16 14:17:02 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Paint from './Paint';
 import About from './About';
+import Resources from './Resources';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Navigate to="/paint" />} />
           <Route path="/paint" element={<Paint />} />
           <Route path="/about" element={<About />} />
+          <Route path="/about" element={<Resources />} />
         </Routes>
       </div>
     </Router>
